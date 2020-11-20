0619202187abb9b450cc7eaaa0917e3f5d57592c
Simon Hutchinson <simonhutc@gmail.com>
2020-11-20 16:51:00 +1100

    Initial Commit

---
+++ alamofiretest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  alamofiretest
+//
+//  Created by SIMON HUTCHINSON on 20/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ alamofiretest/alamofiretestApp.swift
@@ -0,0 +1,17 @@
+//
+//  alamofiretestApp.swift
+//  alamofiretest
+//
+//  Created by SIMON HUTCHINSON on 20/11/20.
+//
+
+import SwiftUI
+
+@main
+struct alamofiretestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
